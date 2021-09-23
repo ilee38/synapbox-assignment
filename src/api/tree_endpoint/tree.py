a7d8561e6348acd5e03110ea28249bbf05ad83a1
Iram Lee <iram.lee@gmail.com>
2021-09-23 05:16:48 -0500

    implement POST request handling

--- src/api/tree_endpoint/tree.py
+++ src/api/tree_endpoint/tree.py
@@ -1,5 +1,6 @@
 from flask import make_response, jsonify, request, json, abort
 from flask.views import MethodView
+import uuid
 
 class Tree(MethodView):
    """API end-point to perform operations on the tree data.
@@ -25,14 +26,53 @@ class Tree(MethodView):
 
    def post(self):
       """Handles POST requests: inserts a new node in the tree.
-         returns: a message indicating the result of the operation.
+         returns: the id of the newly inserted node
       """
       try:
          data = dict(request.json)
+         new_id = str(uuid.uuid4())
 
-         #TODO: implement new node insertion
+         self.insert_node(new_id, data)
 
-         return make_response('Data inserted correctly', 200)
+         return make_response(jsonify({"id": new_id}), 201)
       except Exception as error:
          print(error)
          abort(400)
+
+
+   def insert_node(self, new_id, data):
+      parent_id = data['parent']
+      new_label = data['label']
+
+      with open('tree_data.json') as f:
+         tree_data = json.load(f)
+
+      for element in tree_data:
+         if isinstance(element, dict):
+            self.search_dict(element, parent_id, new_id, new_label)
+         elif isinstance(element, list):
+            self.search_list(element, parent_id, new_id, new_label)
+
+      self.save_tree(tree_data)
+
+
+   def search_dict(self, dict_object, parent_id, new_id, new_label):
+      for element in dict_object:
+         if element == parent_id:
+            dict_object[element]['children'].append({new_id: {'label': new_label, 'children': []}})
+            break
+         elif isinstance(dict_object[element], dict):
+            self.search_dict(dict_object[element], parent_id, new_id, new_label)
+         elif isinstance(dict_object[element], list):
+            self.search_list(dict_object[element], parent_id, new_id, new_label)
+
+
+   def search_list(self, list_object, parent_id, new_id, new_label):
+      for i in range(len(list_object)):
+         if isinstance(list_object[i], dict):
+            self.search_dict(list_object[i], parent_id, new_id, new_label)
+
+
+   def save_tree(self, tree_data):
+      with open('tree_data.json', 'w') as f:
+         f.write(json.dumps(tree_data))
